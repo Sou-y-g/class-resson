21fc2bff9030c417934ed7edc6c963db641e3db3
Sou Yonamine <souyonamine@SounoMacBook-Pro.local>
2022-05-04 17:19:13 +0900

    なぜか結果が必ず引き分けになる。原因追求はできてない

--- war.py
+++ war.py
@@ -1,9 +1,7 @@
 # カードのクラス定義
 
-from telnetlib import XASCII
 
-
-class Card():
+class Card:
 
     marks = ["spades","hearts","diamonds","clubs"]
     numbers = [None,None,"2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"]
@@ -45,13 +43,68 @@ from random import shuffle
 class Deck:
      
     def __init__(self):
-        self.cards_list = []
+        self.cards = []
         for i in range(2,15):
             for j in range(4):
-                self.cards_list.append((i,j))
-                shuffle(self.cards_list)
+                self.cards.append((i,j))
+        shuffle(self.cards)
 
     def rm_card(self):
-        if len(self.cards_list) == 0:
+        if len(self.cards) == 0:
             return
-        return self.cards_list.pop()
+        return self.cards.pop()
+
+
+class Player:
+    def __init__(self,name):
+        self.wins = 0
+        self.name = name
+        self.card = None
+
+class Game:
+    def __init__(self):
+        name1 = input("プレイヤー1の名前を入力してください。")
+        name2 = input("プレイヤー2の名前を入力してください。")
+        self.deck = Deck()
+        self.p1 = Player(name1)
+        self.p2 = Player(name2)
+
+    def wins(self,winner):
+        print(f"このラウンドは{winner}が勝ちました")
+
+    def draw(self,p1n,p1c,p2n,p2c):
+        print(f"{p1n}は{p1c}を引きました、{p2n}は{p2c}を引きました")
+        
+    def player_game(self):
+        cards = self.deck.cards
+        print("ゲームを始めます")
+        while len(cards) >= 2:
+            m = input("ゲームを終了しますか？q:終了")
+            if m == "q":
+                break
+            p1c = self.deck.rm_card()
+            p2c = self.deck.rm_card()
+            p1n = self.p1.name
+            p2n = self.p2.name
+
+            self.draw(p1n,p1c,p2n,p2c)
+
+            if p1c > p2c:
+                self.p1.wins += 1
+                self.wins(self.p1.name)
+            else:
+                self.p2.wins += 1
+                self.wins(self.p2.name)
+
+        win = self.winner(self.p1, self.p2)
+        print(f"ゲーム終了、このゲームは{win}の勝利です。")
+
+    def winner(self, p1, p2):
+        if p1.wins > p2.wins:
+            return p1.name
+        if p2.wins > p1.wins:
+            return p2.name
+        return "引き分け"
+
+game = Game()
+game.player_game()
