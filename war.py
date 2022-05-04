e2a0a261fbb4d2917f377bdc7f332b02defdcdd4
Sou Yonamine <souyonamine@SounoMacBook-Pro.local>
2022-05-04 15:20:41 +0900

    Deckクラス作成

--- war.py
+++ war.py
@@ -3,8 +3,55 @@
 from telnetlib import XASCII
 
 
-class Car():
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
+class Card():
+
+    marks = ["spades","hearts","diamonds","clubs"]
+    numbers = [None,None,"2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"]
+
+    def __init__(self,n,m):
+        self.number = n
+        self.mark = m
+
+    def __it__(self, c):
+        if self.number < c.number:
+            return True
         
+        if self.number == c.nuber:
+            if self.mark < c.number:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def __gt__(self, c):
+        if self.number > c.number:
+            return True
+        
+        if self.number == c.nuber:
+            if self.mark > c.number:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def __repr__(self):
+         n = self.numbers[self.number] + " of " + self.marks[self.mark]
+         return n
+
+from random import shuffle
+
+class Deck:
+     
+    def __init__(self):
+        self.cards_list = []
+        for i in range(2,15):
+            for j in range(4):
+                self.cards_list.append((i,j))
+                shuffle(self.cards_list)
+
+    def rm_card(self):
+        if len(self.cards_list) == 0:
+            return
+        return self.cards_list.pop()
